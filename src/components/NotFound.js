21c4a8cff62a941303694e548bbd9bee2502b61b
marceltheshell <marcel.degas@gmail.com>
2021-08-31 16:32:59 -0700

    adds not found component

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function NotFound () {
+	return (
+		<Container>
+			<h2>Oops, nothing to see here!</h2>
+		</Container>
+	);
+}
+
+export default NotFound;
