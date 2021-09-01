0d5b75f4bf81654693b41245851bca4bb6579a49
marceltheshell <marcel.degas@gmail.com>
2021-09-01 16:31:21 -0700

    adds token logic to apiclient

--- src/clients/SeatMasterApiClient.js
+++ src/clients/SeatMasterApiClient.js
@@ -1,19 +1,40 @@
 const axios = require('axios').default;
 
+const setConfig = (authToken) => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+			...(authToken && {'Authorization': authToken})
+		} 
+	};
+	console.log('config: ', config);
+	return config;
+};
+
 const SeatMasterApiClient = {
-	get: async (url, queryParams) => {
-		// todo
-		console.log(url, queryParams);
-	},
-	post: async (url, bodyParams) => {
-		const headers = {
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		};
+	get: async (url, queryParams, authToken) => {
 		
+		const fullUrl = `${url}/${queryParams}`;
+		console.log('Getting with url: ', fullUrl);
+
+		const config = setConfig(authToken);
+
 		try {
-			const resp = await axios.post(url, bodyParams, headers);
+			const resp = await axios.get(fullUrl, config);
+			console.log('Get response', resp);
+			return resp;
+		} catch (err) {
+			console.error('error in Get response', err);
+			return err;
+		}
+	},
+	post: async (url, bodyParams, authToken) => {
+		const config = setConfig(authToken);
+		console.log('Posting with url: ', url);
+		console.log('Posting with params: ', bodyParams);
+
+		try {
+			const resp = await axios.post(url, bodyParams, config);
 			console.log('Post response', resp);
 			return resp;
 		} catch (err) {
