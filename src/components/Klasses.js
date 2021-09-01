42d3a78e4fc93743f8b472e923a406623103bb71
marceltheshell <marcel.degas@gmail.com>
2021-09-01 16:31:42 -0700

    adds get schools to Klasses

--- src/components/Klasses.js
+++ src/components/Klasses.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import NewHeader from './NewHeader';
+import SeatMasterApiClient from '../clients/SeatMasterApiClient';
+import { useAuth } from '../context/AuthContext';
 
 function Klasses () {
+	const { currentUser } = useAuth();
+	const hasFetchedSchools = useRef(false);
+	//const [klasses, setKlasses] = useState([]);
+	const [schools, setSchools] = useState([]);
+
+	
+
+	const fetchSchools = async () => {
+		if (!hasFetchedSchools.current) {
+			const getSchoolsUrl = `${process.env.REACT_APP_DEV_SERVER_URL}/api/user_schools`;
+			
+			const schools = await SeatMasterApiClient.get(getSchoolsUrl, currentUser.id, currentUser.authToken );
+		
+			setSchools(schools);
+
+			hasFetchedSchools.current = true;
+		}
+	};
+
+	useEffect (() => {
+		fetchSchools();
+	}, [fetchSchools]);
+	
+	console.log(1111, schools);
 	return (
 		<React.Fragment>
 			<NewHeader />
-			<p>klasses page</p>
+			<p>madafakin klasses page</p>
 		</React.Fragment>
 		
 	);
