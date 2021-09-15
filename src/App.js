a06819fbd2a81065b7f0c68dda55781fc63ee3cf
marceltheshell <marcel.degas@gmail.com>
2021-09-15 14:16:24 -0700

    adds react router dom package

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Klasses from './components/Klasses';
 import Welcome from './components/Welcome';
 //import FormExample from './components/FormExample';
 import NotFound from './components/NotFound';
-// import Klass from './Klass';
+import Klass from './components/Klass';
 
 function App() {
 	const user = JSON.parse(sessionStorage.getItem('user'));
@@ -17,7 +17,7 @@ function App() {
 					<Switch>
 						<Route exact path="/" component={Welcome}/>
 						<Route exact path="/klasses" component={Klasses}/>
-						{/* <Route path="/klasses/:id" component={Klass}/> */}
+						<Route path="/klasses/:id" component={Klass}/>
 						<Route component={NotFound} />
 					</Switch>
 				</div>
