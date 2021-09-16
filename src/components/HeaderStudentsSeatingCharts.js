ba5fe4f2b374a1460a578cc24863632e3e563e87
marceltheshell <marcel.degas@gmail.com>
2021-09-16 12:20:53 -0700

    adds icons to sc header

--- src/components/HeaderStudentsSeatingCharts.js
+++ src/components/HeaderStudentsSeatingCharts.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Navbar, Nav, Container, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
+import { PlusCircle } from 'react-bootstrap-icons';
 
 function HeaderStudentsSeatingCharts ( props ) {
-	const { setStudentsView } = props;
+	const { setStudentsView, studentsView, handleShowAddSeatingChart } = props;
+	
 	const handleChange = (val) => {
 		if (val === 1) setStudentsView(true);
 		if (val === 2) setStudentsView(false);
 	};
+	
 	return (
 		<Container>
 			<Navbar className="navbarClass" expand="lg">
 				<Nav className="m-auto">
-					<Nav.Item className="d-flex align-items-center">
+					<Nav.Item className="d-flex align-items-center ">
 						<ToggleButtonGroup type="radio" name="options" defaultValue={1} onChange={handleChange}>
 							<ToggleButton id="tbg-btn-1" value={1}>
 								Students
@@ -21,7 +24,31 @@ function HeaderStudentsSeatingCharts ( props ) {
 							</ToggleButton>
 						</ToggleButtonGroup>
 					</Nav.Item>
+					{!studentsView && <Nav.Item className="d-flex align-items-center ">
+						<h3
+							onClick={handleShowAddSeatingChart}
+						>
+							<PlusCircle />
+						</h3>
+					</Nav.Item>}
+					{!studentsView && <Nav.Item className="d-flex align-items-center ">
+						<h3>
+							<PlusCircle />
+						</h3>
+					</Nav.Item>}
+					{!studentsView && <Nav.Item className="d-flex align-items-center ">
+						<ToggleButtonGroup type="radio" name="options">
+							<ToggleButton>
+								Seating Charts
+							</ToggleButton>
+						</ToggleButtonGroup>
+					</Nav.Item>}
 				</Nav>
+				{/* <Nav className="ms-auto">
+					<Nav.Item className="d-flex align-items-center">
+						hello there darius the great
+					</Nav.Item>
+				</Nav> */}
 			</Navbar>
 		</Container>
 	);
